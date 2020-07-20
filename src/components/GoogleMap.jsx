3eadb3df5ef2864c7449c8bac4761ea82e7d93c0
Cristin O'Connor <her@cristin.io>
2020-07-20 06:15:57 -0600

    Google Maps included too many times - need to fix

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -4,19 +4,17 @@ const MAPS_API_KEY = 'AIzaSyCSXTUEm9DgP-MHuHdbiIxaWZj12z7sq1I';
 
 const GoogleMap = ({ lat, lon }) => {
   const mapEl = useRef(null);
-  console.log(lat)
-  console.log(lon)
 
-  const renderMap = async(ref) => {
-
+  const renderMap = async() => {
     const loader = new Loader(MAPS_API_KEY);
     const google = await loader.load();
-  /**
+
+    /**
     * new google.maps.Map is how we create an embedded google map inside our document
     * google.maps.map takes a reference to a DOM node (this.refs.map), then
     * it finds that element on the screen, and renders an embedded map into it
     */
-    const map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(mapEl.current, {
     /**
       * Level 12 zoom allows zooming in enough to get good glimpse of city
       */
@@ -26,11 +24,10 @@ const GoogleMap = ({ lat, lon }) => {
         lng: lon
       }
     });
-    console.log(map)
   }
 
   useEffect(() => {
-    renderMap(mapEl);
+    renderMap();
   })
   
   return <div ref={mapEl} id='map' style={{position: 'static', minHeight: '300px', width: '100%'}}></div>;
