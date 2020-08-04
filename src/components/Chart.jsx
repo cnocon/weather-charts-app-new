79bc8c3263d23be659cd213bcbb22640b260dbe0
Cristin O'Connor <her@cristin.io>
2020-08-03 19:53:44 -0600

    Added more detail to charts

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 // Sparklines chart docs http://borisyankov.github.io/react-sparklines/
-import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine, SparklinesNormalBand } from 'react-sparklines';
 import _ from 'lodash';
 
-const average = (data) => {
+const average = data => {
   return _.round(_.sum(data)/data.length);
+}
+
+const max = data => {
+  return _.round(_.max(data));
+}
+
+const min = data => {
+  return _.round(_.min(data));
 }
 
 export default (props) => {
   return (
     <div>
-      <h6><em>Avg. {average(props.data)}{props.units}</em></h6>
+      <h6><em>Avg. {average(props.data)}{props.units}<br/>High: {max(props.data)}{props.units} / Low: {min(props.data)}{props.units}</em></h6>
       <Sparklines height={120} width={180} data={props.data}>
-        <SparklinesLine style={{ stroke: `${props.color}`, fill: `${props.color}`, fillOpacity: "0.45" }}/>
+        <SparklinesLine style={{ stroke: `${props.color}`, fill: `${props.color}`, fillOpacity: ".25"}}/>
         <SparklinesReferenceLine type="avg"/>
       </Sparklines>
     </div>
